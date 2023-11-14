ae7b5db54777ec79159c637a3168bec7385cb574
pdoazan <paul.doazan@gmail.com>
2023-11-14 15:57:19 +0100

    WIP flower infos page

--- src/components/exploration/Explore.jsx
+++ src/components/exploration/Explore.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import dataFlowers from '../../assets/data/flowers.json'
 import FlowerCard from './FlowerCard'
 import FlowerInfos from './FlowerInfos'
--- src/components/exploration/FlowerInfos.jsx
+++ src/components/exploration/FlowerInfos.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import dataFlowers from '../../assets/data/flowers.json'
 import { LangContext } from '../../utils/context'
+import navigation_configs from '../../config/navigation_configs.json'
 
 export default function FlowerInfos({ data }) {
     const { lang } = useContext(LangContext)
@@ -9,6 +10,9 @@ export default function FlowerInfos({ data }) {
     const handleTouch = (e) => {
         e.stopPropagation()
     }
+
+    console.log(currentFlower[lang].family);
+
     return (
         <div onTouchStart={handleTouch} onTouchMove={handleTouch} onTouchEnd={handleTouch} className='flower__info__container'>
             <div className="flower__info__img__container">
@@ -27,37 +31,75 @@ export default function FlowerInfos({ data }) {
 
                 <div className="flower__info__contents">
                     <div className="flower__info__caracteristics">
-                        <div className="flower__info__caracteristics__icons">
-                            <div className="flower__info__family__icon flower__info__icon">
-                                <img src="/images/icons/family.svg" alt="" />
+                        <div className="flower__info__caracteristics__section">
+                            <div className="flower__info__family__title flower__info__caracteristics__titles__element">
+                                <span className="flower__info__family__icon flower__info__icon"><img src="/images/icons/family.svg" alt="" /></span>
                             </div>
-                            <div className="flower__info__bloom__icon flower__info__icon">
-                                <img src="/images/icons/bloom.svg" alt="" />
+                            <div className="flower__info__bloom__title flower__info__caracteristics__titles__element">
+                                <span className="flower__info__bloom__icon flower__info__icon"><img src="/images/icons/bloom.svg" alt="" /></span>
                             </div>
-                            <div className="flower__info__height__icon flower__info__icon">
-                                <img src="/images/icons/height.svg" alt="" />
+                            <div className="flower__info__height__title flower__info__caracteristics__titles__element">
+                                <span className="flower__info__height__icon flower__info__icon"><img src="/images/icons/height.svg" alt="" /></span>
                             </div>
-                            <div className="flower__info__habitat__icon flower__info__icon">
-                                <img src="/images/icons/habitat.svg" alt="" />
+                            <div className="flower__info__habitat__title flower__info__caracteristics__titles__element">
+                                <span className="flower__info__habitat__icon flower__info__icon"><img src="/images/icons/habitat.svg" alt="" /></span>
                             </div>
                         </div>
 
-                        <div className="flower__info__caracteristics__titles">
-                            <div className="flower__info__family__title flower__info__caracteristics__titles__element">Famille</div>
-                            <div className="flower__info__bloom__title flower__info__caracteristics__titles__element">Floraison</div>
-                            <div className="flower__info__height__title flower__info__caracteristics__titles__element">Hauteur</div>
-                            <div className="flower__info__habitat__title flower__info__caracteristics__titles__element">Habitat</div>
+                        <div className="flower__info__caracteristics__section caracteristics__section__2">
+                            <div className="flower__info__family__title flower__info__caracteristics__titles__element">
+                                <span className="flower__info__real__title">Family</span>
+                                <span className="flower__info__family__icon flower__info__icon fake__icon"><img src="/images/icons/family.svg" alt="" /></span>
+
+                            </div>
+                            <div className="flower__info__bloom__title flower__info__caracteristics__titles__element">
+                                <span className="flower__info__real__title">Bloom</span>
+                                <span className="flower__info__bloom__icon flower__info__icon fake__icon"><img src="/images/icons/bloom.svg" alt="" /></span>
+                            </div>
+                            <div className="flower__info__height__title flower__info__caracteristics__titles__element">
+                                <span className="flower__info__real__title">Height</span>
+                                <span className="flower__info__height__icon flower__info__icon fake__icon"><img src="/images/icons/height.svg" alt="" /></span>
+                            </div>
+                            <div className="flower__info__habitat__title flower__info__caracteristics__titles__element">
+                                <span className="flower__info__real__title">Habitat</span>
+                                <span className="flower__info__habitat__icon flower__info__icon fake__icon"><img src="/images/icons/habitat.svg" alt="" /></span>
+                            </div>
                         </div>
 
-                        <div className="flower__info__caracteristics__contents">
-                            <div className="flower__info__family__content"></div>
-                            <div className="flower__info__bloom__content"></div>
-                            <div className="flower__info__height__content"></div>
-                            <div className="flower__info__habitat__content"></div>
+                        <div className="flower__info__caracteristics__section caracteristics__section__3">
+                            <div className="flower__info__family__title flower__info__caracteristics__titles__element">
+                                <span className="flower__info__real__title">{currentFlower[lang].family}</span>
+                                <span className="flower__info__icon fake__icon"><img src="/images/icons/family.svg" alt="" /></span>
+                            </div>
+                            <div className="flower__info__bloom__title flower__info__caracteristics__titles__element">
+                                <span className="flower__info__real__title">{currentFlower[lang].bloom}</span>
+                                <span className="flower__info__icon fake__icon"><img src="/images/icons/bloom.svg" alt="" /></span>
+                            </div>
+                            <div className="flower__info__height__title flower__info__caracteristics__titles__element">
+                                <span className="flower__info__real__title">{currentFlower[lang].height}</span>
+                                <span className="flower__info__icon fake__icon"><img src="/images/icons/height.svg" alt="" /></span>
+                            </div>
+                            <div className="flower__info__habitat__title flower__info__caracteristics__titles__element">
+                                <span className="flower__info__real__title">{currentFlower[lang].habitat}</span>
+                                <span className="flower__info__icon fake__icon"><img src="/images/icons/habitat.svg" alt="" /></span>
+                            </div>
                         </div>
+
+                        <div className="flower__info__specification">
+                            <div className="flower__info__specification__section">
+                                <p className="flower__info__specification__title">{navigation_configs.flowerInfos[lang].medical}</p>
+                                <p className="flower__info__specification__content">{currentFlower[lang].medical}</p>
+                                <p className="flower__info__specification__content">{currentFlower[lang].medical}</p>
+                            </div>
+                            <div className="">
+                                <p className="flower__info__specification__title">{navigation_configs.flowerInfos[lang].cooking}</p>
+                                <p className="flower__info__specification__content">{currentFlower[lang].cooking}</p>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
