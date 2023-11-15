6f62ae47367169d05eeb6fa8364d9271816e0dd2
pdoazan <paul.doazan@gmail.com>
2023-11-16 00:24:35 +0100

    Add preload images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Frame from './components/Frame';
 import { NavigationProvider } from './utils/context';
+import useImagePreloader from './hooks/useImagePreloader'
+
+const imgs = [
+  'images/flowers/full/achillea_millefolium.jpg',
+  'images/flowers/full/alchemilla_alpina.jpg',
+  'images/flowers/full/angelica_sylvestris.jpg',
+  'images/flowers/full/arnica_montana.jpg',
+  'images/flowers/full/artemisia_eriantha.jpg',
+  'images/flowers/full/carum_carvi.jpg',
+  'images/flowers/full/digitalis_grandiflora.jpg',
+  'images/flowers/full/euphrasia_minima.jpg',
+  'images/flowers/full/gentiana_lutea.jpg',
+  'images/flowers/full/geranium_robertianum.jpg',
+  'images/flowers/full/hypericum_richeri.jpg',
+  'images/flowers/full/leontodon_pyrenaicus.jpeg',
+  'images/flowers/full/origanum_vulgare.jpg',
+  'images/flowers/full/papaver_rhoeas.jpg',
+  'images/flowers/full/plantago_major.jpg',
+  'images/flowers/full/polygonatum_odoratum.jpg',
+  'images/flowers/full/rhyteuma_spicatum.jpg',
+  'images/flowers/full/rumex_pseudalpinus.jpg',
+  'images/flowers/full/saxifraga_stellaris.jpg',
+  'images/flowers/full/sedum_album.jpg',
+  'images/flowers/full/sempervivum_montanum.jpg',
+  'images/flowers/full/spirea_ulmaria.jpg',
+  'images/flowers/full/thymus_serpyllum.jpg',
+  'images/flowers/full/tussilago_farfara.jpg',
+  'images/flowers/full/viola_canina.jpg',
+]
 
 function App() {
   const [nbScreen, setNbScreen] = useState(1)
+  const { imagesPreloaded } = useImagePreloader(imgs)
 
   const screens = []
 
@@ -27,12 +57,11 @@ function App() {
 
   return (
     <>
-
-      <div className={`screens__container__${screens.length}`}>
+      {imagesPreloaded ? <div className={`screens__container__${screens.length}`}>
         {screens.map((el, index, arr) => {
           return <NavigationProvider><Frame key={index} id={index} arrLength={arr.length} /></NavigationProvider>
         })}
-      </div>
+      </div> : <div>IS LOADING</div>}
 
       {/* <select name="nbScreens" id="nbScreen-select" className='nbScreen__select' onChange={handleNbScreenChange}>
         <option value="1">1</option>
