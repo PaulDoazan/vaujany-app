9e6cc015642be472311bb76d6e2c8a78d17c7424
pdoazan <paul.doazan@gmail.com>
2023-11-25 22:49:37 +0100

    WIP

--- src/components/games/gamesComponents/guesswho/GuessWho.jsx
+++ src/components/games/gamesComponents/guesswho/GuessWho.jsx
@@ -1,11 +1,57 @@
 import gsap from 'gsap'
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import flowersData from '../../../../assets/data/flowers.json'
+import { shuffleArray } from '../../../../utils/utils'
+import { NavigationContext } from '../../../../utils/context'
+import GuessWhoCard from './GuessWhoCard'
+
+const layouts = {
+    level_0: {
+        nbCards: 9,
+        rows: 3,
+        cols: 3,
+        origin: { x: 13.4, y: 26.8 }
+    },
+    level_1: {
+        nbCards: 12,
+        rows: 4,
+        cols: 3,
+        origin: { x: 13.4, y: 14.1 }
+    },
+    level_2: {
+        nbCards: 16,
+        rows: 4,
+        cols: 4,
+        origin: { x: 8, y: 14.1 }
+    }
+}
 
 export default function GuessWho() {
+    const [deck, setDeck] = useState([])
+    const { currentPage } = useContext(NavigationContext)
+
+    const restart = () => {
+        let flowers = flowersData.flowers.filter(el => el.guessWhoParameters)
+        shuffleArray(flowers)
+        flowers = flowers.splice(0, layouts[`level_${currentPage.level}`].nbCards)
+
+        setDeck(flowers)
+    }
+
+    const handleTouchStart = (e) => {
+        console.log(e.currentTarget);
+        e.currentTarget.classList.toggle('guesswho__is__flipped')
+    }
+
     useEffect(() => {
+        restart()
         gsap.fromTo('.guesswho__container', { opacity: 0 }, { opacity: 1, duration: 0.5 })
     }, [])
     return (
-        <div className='guesswho__container'>GuessWho</div>
+        <div className='guesswho__container'>
+            {deck.map((el, index) => {
+                return <GuessWhoCard key={index} flower={el} index={index} handleTouchStart={handleTouchStart} layout={layouts[`level_${currentPage.level}`]} />
+            })}
+        </div>
     )
 }
---
+++ src/components/games/gamesComponents/guesswho/GuessWhoCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function GuessWhoCard({ flower, index, layout, handleTouchStart }) {
+    const width = 10.7;
+    const height = 14.3
+    const gapX = 2.6
+    const gapY = gapX * 16 / 9
+    const top = (height + gapY) * (Math.floor(index / (layout.cols)))
+
+    const cardStyle = {
+        transition: 'transform 0.7s',
+        transformStyle: 'preserve-3d',
+        width: `${width}%`,
+        height: `${height}%`,
+        position: 'absolute',
+        left: `${layout.origin.x + (index % (layout.cols)) * (width + gapX)}%`,
+        top: `${layout.origin.y + top}%`
+    }
+
+    return (
+        <div className="guesswho__card guesswho__is__flipped" style={cardStyle} onTouchEnd={handleTouchStart}>
+            <div className="guesswho__card__face guesswho__card__face__back" >
+                <img className='guesswho__card__image__back' src={`images/guesswho/imgBack.png`} alt="" />
+            </div>
+            <div className="guesswho__card__image guesswho__card__face">
+                <img className='guesswho__card__flower__image' src={`images/flowers/thumbnails/${flower.thumbnail}`} alt="" />
+            </div>
+        </div>
+    )
+}
